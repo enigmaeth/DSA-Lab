01172c4995e99c5906afbce4848ccc8192d21795
enigmaeth <enigmaeth@gmail.com>
2017-01-21 17:00:51 +0530

    added memset

--- f2015119/Lab1_20Jan/prob2.c
+++ f2015119/Lab1_20Jan/prob2.c
@@ -6,7 +6,11 @@ int solve()
 {
 	char res[1000];
 	char a[1000],b[1000],c[1000];
-	scanf(" %s",&res);
+    memset(a, 0, 1000);
+    memset(b, 0, 1000);
+    memset(c, 0, 1000);        
+
+    scanf(" %s",&res);
 	int n = strlen(res);
 	int start=0,sqroot=0,found=0,i=0,j=0,k=0;
 
@@ -43,29 +47,23 @@ int solve()
 		{
 			for(j=n-1;j>=0;j--)
 			{
-				if(k<strlen(res))
-				{
-					b[k]=ar[i][j];
-					k++;
-				}
+                b[k]=ar[i][j];
+                k++;
 			}
-		}		
+		}
 		k=0;
 		for(i=n-1;i>=0;i--)
 		{
 			for(j=n-1;j>=0;j--)
 			{
-				if(k<(n*n))
-				{
-					c[k]=ar[j][i];
-					k++;
-				}
-				else
-					c[k]='\0';
+				c[k]=ar[j][i];
+			
+				printf("%c",c[k]);
+			k++;				
 			}
 		}
 
-		printf("%s\n %s\n",c,b);
+		//printf("**%s**%s**%s**",a,b,c);
 
 		if((strcmp(res,a)==0)&&(strcmp(res,b)==0)&&(strcmp(res,c)==0))
 			printf("YES\n");
